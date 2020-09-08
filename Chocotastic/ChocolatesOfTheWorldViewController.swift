c57346787dc26f2c19678578ff4f9fe4c6600662
Sumair Zamir <sumair.zamir@gmail.com>
2020-09-08 23:29:04 +0100

    next steps: rxswift observables & rxcocoa implementation

--- Chocotastic/ChocolatesOfTheWorldViewController.swift
+++ Chocotastic/ChocolatesOfTheWorldViewController.swift
@@ -33,7 +33,11 @@ import RxCocoa
 class ChocolatesOfTheWorldViewController: UIViewController {
   @IBOutlet private var cartButton: UIBarButtonItem!
   @IBOutlet private var tableView: UITableView!
-  let europeanChocolates = Chocolate.ofEurope
+  // Sets the property as an observable
+  // Just indicates no changes will be made to underlying value of the observable
+  // Using just may suggest that an Rx implementation is overkill - case specific
+  let europeanChocolates = Observable.just(Chocolate.ofEurope)
+  private let disposeBag = DisposeBag()
 }
 
 //MARK: View Lifecycle
@@ -42,61 +46,120 @@ extension ChocolatesOfTheWorldViewController {
     super.viewDidLoad()
     title = "Chocolate!!!"
     
-    tableView.dataSource = self
-    tableView.delegate = self
+//    tableView.dataSource = self
+//    tableView.delegate = self
+    
+    setupCartObserver()
+    setupCellConfiguration()
+    setupCellTapHandling()
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    updateCartButton()
+//    updateCartButton()
   }
 }
 
 //MARK: - Rx Setup
 private extension ChocolatesOfTheWorldViewController {
   
+  func setupCartObserver() {
+    //1
+    // This creates an observable on chocolates in the shopping cart
+    ShoppingCart.sharedCart.chocolates.asObservable()
+      .subscribe(onNext: { //2
+        // For ARC purposes and retain cycle management re: unowned self
+        // Everytime the chocolate variable changes a notification is sent to the observer
+        // Each time this notification is received the count is updated
+        [unowned self] chocolates in
+        self.cartButton.title = "\(chocolates.count) 🍫"
+      })
+      // The retain cycle is disposed
+      .disposed(by: disposeBag) //3
+  }
+  
+  func setupCellConfiguration() {
+    ///
+    europeanChocolates
+      // Bind the europeanChocolates observable to the tableView
+      .bind(to: tableView
+        // Access the RxCocoa extension
+        .rx
+        // Specify the cell identifier similar to UITableViewDataSource
+        .items(cellIdentifier: ChocolateCell.Identifier,
+               cellType: ChocolateCell.self)) {
+                // For each row, pass in the following information
+                row, chocolate, cell in
+                cell.configureWithChocolate(chocolate: chocolate)
+    }
+    .disposed(by: disposeBag)
+  }
+  
+  func setupCellTapHandling() {
+    tableView
+      .rx
+      // Passes in a Chocolate model type as an observable
+      .modelSelected(Chocolate.self)
+      // What to do when a model is selected, i.e. a row is selected
+      .subscribe(onNext: { [unowned self] chocolate in
+        let newValue = ShoppingCart.sharedCart.chocolates.value + [chocolate]
+        // Add the selected chocolate to the cart - this also leads to an increment of the count
+        ShoppingCart.sharedCart.chocolates.accept(newValue)
+        
+        // Deselect the tapped row
+        if let selectedRowIndexPath = self.tableView.indexPathForSelectedRow {
+          self.tableView.deselectRow(at: selectedRowIndexPath, animated: true)
+        }
+      })
+    .disposed(by: disposeBag)
+  }
+  
+  
 }
 
 //MARK: - Imperative methods
 private extension ChocolatesOfTheWorldViewController {
-  func updateCartButton() {
-    cartButton.title = "\(ShoppingCart.sharedCart.chocolates.count) 🍫"
-  }
+//  func updateCartButton() {
+//    //    cartButton.title = "\(ShoppingCart.sharedCart.chocolates.count) 🍫"
+//    cartButton.title = "\(ShoppingCart.sharedCart.chocolates.value.count) 🍫"
+//  }
 }
 
 // MARK: - Table view data source
-extension ChocolatesOfTheWorldViewController: UITableViewDataSource {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return europeanChocolates.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: ChocolateCell.Identifier, for: indexPath) as? ChocolateCell else {
-      //Something went wrong with the identifier.
-      return UITableViewCell()
-    }
-    
-    let chocolate = europeanChocolates[indexPath.row]
-    cell.configureWithChocolate(chocolate: chocolate)
-    
-    return cell
-  }
-}
+//extension ChocolatesOfTheWorldViewController: UITableViewDataSource {
+//  func numberOfSections(in tableView: UITableView) -> Int {
+//    return 1
+//  }
+//
+//  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    return europeanChocolates.count
+//  }
+//
+//  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    guard let cell = tableView.dequeueReusableCell(withIdentifier: ChocolateCell.Identifier, for: indexPath) as? ChocolateCell else {
+//      //Something went wrong with the identifier.
+//      return UITableViewCell()
+//    }
+//
+//    let chocolate = europeanChocolates[indexPath.row]
+//    cell.configureWithChocolate(chocolate: chocolate)
+//
+//    return cell
+//  }
+//}
 
 // MARK: - Table view delegate
-extension ChocolatesOfTheWorldViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-    
-    let chocolate = europeanChocolates[indexPath.row]
-    ShoppingCart.sharedCart.chocolates.append(chocolate)
-    updateCartButton()
-  }
-}
+//extension ChocolatesOfTheWorldViewController: UITableViewDelegate {
+//  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    tableView.deselectRow(at: indexPath, animated: true)
+//
+//    let chocolate = europeanChocolates[indexPath.row]
+//    //    ShoppingCart.sharedCart.chocolates.append(chocolate)
+//    let newValue = ShoppingCart.sharedCart.chocolates.value + [chocolate]
+//    ShoppingCart.sharedCart.chocolates.accept(newValue)
+////    updateCartButton()
+//  }
+//}
 
 // MARK: - SegueHandler
 extension ChocolatesOfTheWorldViewController: SegueHandler {
